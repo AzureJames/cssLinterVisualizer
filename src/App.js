ad95662538b922a4594d0dd051338b9eabf165f1
AzureJames <azurethegreat@gmail.com>
2022-11-06 08:41:16 -0700

    style

--- src/App.js
+++ src/App.js
@@ -6,6 +6,47 @@ import SankeyChart from './components/SankeyChart';
 
 function App() {
 
+  // let sankeyData =  [
+  //   ['From', 'To', 'Weight'],
+  //   ['Header', 'Line-height', .2],
+  //   ['Nav', 'Font-family', .2],
+  //   ['H1', 'Font-size', .2],
+  //   ['H2', 'Text-align', .2],
+  //   ['H3', 'Color', .2],
+  //   ['P', 'Background-color', .2],
+  //   ['Div', 'Background', .2],
+  //   ['Article', 'Height', .2],
+  //   ['Img', 'Width', .2],
+  //   ['Picture', 'Margin', .2],
+  //   ['Ol', 'Padding', .2],
+  //   ['Ul', 'Gap', .2],
+  //   ['Li', 'Border', .2],
+  //   ['A', 'Display-flex', .2],
+  //   ['Footer', 'Border-radius', .2],
+  //   ['P', 'Overflow', .2],
+  
+  
+  //   ['Line-height', 'Typography', .2],
+  //   ['Font-family', 'Typography', .2],
+  //   ['Font-size', 'Typography', .2],
+  //   ['Text-align', 'Typography', .2],
+  //   ['Background-color', 'Backgrounds', .2],
+  //   ['Background', 'Backgrounds   ', .2],
+  //   ['Height', 'Sizing', .2],
+  //   ['Width', 'Sizing', .2],
+  //   ['Margin', 'Margin & Padding', .2],
+  //   ['Padding', 'Margin & Padding', .2],
+  //   ['Gap', 'Margin & Padding', .2],
+  //   ['Border', 'Borders', .2],
+  //   ['Border-radius', 'Borders', .2],
+  //   ['Display-block', 'Display', .2],
+  //   ['Display-flex', 'Display', .2],
+  //   ['Overflow', 'Display', .2]
+  // ];
+
+
+
+
 
   let selectorsArray = [
 "header",
@@ -52,6 +93,13 @@ let rulesArray = [
   let inputProcessed = "";
   let inputProcessedMini = "";
 
+
+
+
+
+
+
+
   let processCss = () => {
     console.log(inputCss, outputCss, outputCssMini);
 
@@ -84,7 +132,9 @@ let rulesArray = [
             </div>
         </div>
         <div className="css-chart">
+        
           <SankeyChart />
+        
         </div>
         <footer>2022 Azure James</footer>
     </div>
